d0141f28adcf7172bae75668265aabf5b9b1e476
Norman Kong <normankong@gmail.com>
2021-06-17 22:29:52 +0800

    Refactor

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 This is a API service to provide the Fortune Stick drawing and the explanation, host on Cloudflare Worker platform
 
 ### To use directly
-https://che-kung.normankong.workers.dev/divination/  
-https://che-kung.normankong.workers.dev/divination/1  (Range 1 - 96)
+https://che-kung.normankong.workers.dev/  
+https://che-kung.normankong.workers.dev/1  (Range 1 - 96)
 
 ### To Build 
 ```
@@ -16,6 +16,29 @@ npm run pack
 ```
 wrangler dev
 wrangler publish
+```
+### Sample Response
+```json
+{
+    "index": 1,
+    "token": "第一簽",
+    "score": "上簽",
+    "head1": "帥靈地傑逞英豪，一箭分明點大敖：",
+    "head2": "得意春風攜滿袖，馬頭聲唱狀元高。",
+    "lines": [
+        "家宅：喜事將近",
+        "出行：順順利利",
+        "姻緣：必得美滿姻緣",
+        "行人：必達彼岸",
+        "生育：可得男孩",
+        "訴訟：萬事皆以和為貴",
+        "事業：起步順利",
+        "搬遷：遠近皆宜",
+        "自身：努力者必得其成就",
+        "疾病：小心腸胃",
+        "財運：晚年更佳"
+    ]
+}
 ```
 
 For installation of Cloudflare worker and Wrangler, it can be found [here](https://developers.cloudflare.com/workers/tooling/wrangler).
--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const divinationList = require('./resources/data.json')
-const MIN = 1
-const MAX = 96
+const LIST = require('./resources/data.json');
+const MIN = 1;
+const MAX = 96;
 
+// Main Handler
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
   const { pathname } = new URL(request.url)
-  const uriList = pathname.split('/').slice(1)
+  const uriList = pathname.split('/');
 
-  let number = null
-
-  // Parse the Divination Number if passed
-  const divinationEndpoint = '/divination/'
-  if (pathname.startsWith(divinationEndpoint)) {
-    number = uriList[1]
-  }
+  // Get the first param
+  let number = uriList[1];
 
   // Get the Divination
-  let result = getData(number)
-
-  // Response Mapping
-  let response = result;
+  let response = getDivination(number);
 
   // Return to Caller
   return new Response(JSON.stringify(response, null, ' '), {
@@ -32,16 +25,16 @@ async function handleRequest(request) {
 
 //The maximum is inclusive and the minimum is inclusive
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Get Random Data
-function getData(number) {
-  let index = parseInt(number)
+// Get Divination Data
+function getDivination(number) {
+  let index = parseInt(number);
   if (isNaN(index) || index < MIN || index > MAX) {
-    index = getRandomIntInclusive(MIN, MAX)
+    index = getRandomIntInclusive(MIN, MAX);
   }
-  return divinationList.data[index - 1]
+  return LIST.data[index - 1];
 }
