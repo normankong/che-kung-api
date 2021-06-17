c12b15f2121d56d3e1895bdd3b7bebcfa5285c9b
Norman Kong <normankong@gmail.com>
2021-06-17 14:24:45 +0800

    Refactor

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ async function handleRequest(request) {
   }
 
   // Get the Divination
-  let obj = getData(number)
+  let result = getData(number)
 
   // Response Mapping
-  let response = responseMapping(obj)
+  let response = result;
 
   // Return to Caller
   return new Response(JSON.stringify(response, null, ' '), {
@@ -44,37 +44,4 @@ function getData(number) {
     index = getRandomIntInclusive(MIN, MAX)
   }
   return divinationList.data[index - 1]
-}
-
-// Format Response
-function responseMapping(obj) {
-  let lines = obj.result1 + obj.result2 + obj.result3 + obj.result4
-  lines = lines.replace('自身：', '\n自身：').trim()
-  lines = lines.replace('財運：', '\n財運：').trim()
-  lines = lines.replace('訴訟：', '\n訴訟：').trim()
-  lines = lines.replace('行人：', '\n行人：').trim()
-  lines = lines.replace('生育：', '\n生育：').trim()
-  lines = lines.replace('出行：', '\n出行：').trim()
-  lines = lines.replace('事業：', '\n事業：').trim()
-  lines = lines.replace('姻緣：', '\n姻緣：').trim()
-  lines = lines.replace('家宅：', '\n家宅：').trim()
-  lines = lines.replace('疾病：', '\n疾病：').trim()
-  lines = lines.replace('搬遷：', '\n搬遷：').trim()
-  lines = lines.replace('\n\n', '\n')
-
-  let title = `${obj.token} ${obj.score}`
-  let index = obj.index
-  let type = obj.score
-  let heading = `${obj.line1} ${obj.line2}`
-  let detail = lines
-
-  let response = {
-    index,
-    type,
-    title,
-    heading,
-    detail,
-  }
-
-  return response
 }
